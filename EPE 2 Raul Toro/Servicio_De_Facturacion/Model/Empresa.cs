d4d88dee5b0fefab2469f805826cc35802fd3688
Nashox656 <146797019+Nashox656@users.noreply.github.com>
2023-10-21 16:00:31 -0300

    Add files via upload
    EPE 2

---
+++ EPE 2 Raul Toro/Servicio_De_Facturacion/Model/Empresa.cs
@@ -0,0 +1,22 @@
+//Creamos la clase Empresa para recibir y enviar datos
+
+public class Empresa{
+
+    public string? NombreEmpresa{get;set;}
+
+    public int RutEmpresa{get;set;}
+
+    public string? GiroEmpresa{get;set;}
+
+    public int ID{get;set;}
+
+    public decimal MontoDinero{get;set;}
+
+    public decimal TotalDeVentas{get;set;}
+
+    public float MontoIVA{get;set;}
+
+    public float MontoDeUtilidades{get;set;}
+
+
+}
